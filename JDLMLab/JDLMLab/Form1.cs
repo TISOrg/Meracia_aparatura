46071cc4aad2591e43b7a5854dd995ee144f0908
pavlasek2 <pavlasek2@uniba.sk>
2015-11-13 19:21:27 +0100

    checkbox a hello world :)

--- JDLMLab/JDLMLab/Form1.cs
+++ JDLMLab/JDLMLab/Form1.cs
@@ -16,5 +16,10 @@ namespace JDLMLab
         {
             InitializeComponent();
         }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello world!");
+        }
     }
 }
