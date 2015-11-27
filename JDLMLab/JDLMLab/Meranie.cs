ebac15f91c4eb6864f9c4ca953885dcc46146ea5
pavlasek2 <pavlasek2@uniba.sk>
2015-11-27 17:00:20 +0100

    Trieda Meranie uchovavanie dat potrebnych pre hlavicku merania

---
+++ JDLMLab/JDLMLab/Meranie.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDLMLab
+{
+    /// <summary>
+    ///  Trieda pre jednu hlavicku merania
+    /// Autor - Jano
+    /// </summary>
+    class Meranie
+    {
+        public Meranie(string name,string typMerania,int startPoint,int endPoint,int constant,int resolution,int stepTime,string note,int cycles)
+        {
+            this.name = name;
+            this.startPoint = startPoint;
+            this.endPoint = endPoint;
+            this.name = name;
+            this.constant = constant;
+            this.resolution = resolution;
+            this.stepTime = stepTime;
+            this.note = note;
+            this.cycles = cycles;
+
+        }
+        public Meranie(string name, string typMerania, int startPoint, int endPoint, int constant, int resolution, int stepTime, string note)
+        {
+            this.name = name;
+            this.startPoint = startPoint;
+            this.endPoint = endPoint;
+            this.name = name;
+            this.constant = constant;
+            this.resolution = resolution;
+            this.stepTime = stepTime;
+            this.note = note;
+            this.cycles = 0;    //nula bude reprezentovat nekonecne vela cyklov.
+
+        }
+
+        
+
+        public string typMerania{get;set;}
+
+        public string note { get; set}
+        public string name { get; set; }
+        public DateTime datetime
+        {
+            get; private set;
+        }
+        public int startPoint {
+            get; set; }
+        public int endPoint {
+            get; set}
+        public int constant {
+            get; set; }
+        public int resolution {
+            get; set; }
+        public int stepTime {
+            get; set;
+        }
+        public int cycles {
+            get; set;
+        }
+        public string header { get; set; }
+        
+    }
+}
