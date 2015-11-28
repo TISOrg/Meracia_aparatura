d26437712518722549acfa2ef2182f32d8d83438
pavlasek2 <pavlasek2@uniba.sk>
2015-11-28 18:28:05 +0100

    cyklus merania, a krok merania

---
+++ JDLMLab/JDLMLab/CyklusMerania.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDLMLab
+{
+    class CyklusMerania 
+    {
+        public CyklusMerania(int i)
+        {
+            cisloCyklu = i;
+        }
+        public int cisloCyklu { get; set; }
+        
+        private LinkedList<KrokMerania> kroky;
+        public void pridajKrok(KrokMerania k)
+        {
+            kroky.AddLast(k);
+        }
+        public LinkedList<KrokMerania> getKroky()
+        {
+            return kroky;
+        }
+
+    }
+}
--- JDLMLab/JDLMLab/KrokMerania.cs
+++ JDLMLab/JDLMLab/KrokMerania.cs
@@ -8,9 +8,25 @@ namespace JDLMLab
 {
     class KrokMerania
     {
+        public KrokMerania(int x,int y,int det,int current,int p1,int p2,int temperature)
+        {
+            this.x = x;
+            this.y = y;
+            this.det = det;
+            this.current = current;
+            this.p1 = p1;
+            this.p2 = p2;
+            this.temperature = temperature;
+        }
         public KrokMerania()
         {
-
         }
+        public int x { get; set; }
+        public int y { get; set; }
+        public int det { get; set; }
+        public int current { get; set; }
+        public int p1 { get; set; }
+        public int p2 { get; set; }
+        public int temperature { get; set; }
     }
 }
