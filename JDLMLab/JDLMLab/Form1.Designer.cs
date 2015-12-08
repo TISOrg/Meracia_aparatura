610f878b308c7c37d877fade075aff25bd03abea
LacoWa <ladislav.wagner@gmail.com>
2015-12-08 13:05:29 +0100

    Zmena farby buttonu a funkcne vyskakovacie okno

--- JDLMLab/JDLMLab/Form1.Designer.cs
+++ JDLMLab/JDLMLab/Form1.Designer.cs
@@ -145,13 +145,15 @@
             // 
             // button2
             // 
-            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.Color.Snow;
             this.button2.Location = new System.Drawing.Point(11, 164);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(145, 65);
             this.button2.TabIndex = 0;
             this.button2.Text = "Zastav meranie";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
--- JDLMLab/JDLMLab/Program.cs
+++ JDLMLab/JDLMLab/Program.cs
@@ -16,28 +16,28 @@ namespace JDLMLab
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
-            MeasurementParameters mp = new MeasurementParameters();
-            mp.constant = 4;
-            mp.endPoint = 5;
-            mp.startPoint = 1;
-            mp.name = "nazov";
-            mp.pocetCyklov = 5;
-            mp.stepTime = 10;
-            mp.note = "poznamka ...";
-            
-            Meranie m = new Meranie(mp);
-            m.addKrok(0, new KrokMerania(1, 2, 4, 7, 8, 9, 10));
-            m.addKrok(0, new KrokMerania(2, 4, 4, 7, 8, 9, 10));
-            m.addKrok(0, new KrokMerania(3, 5, 4, 7, 8, 9, 10));
-            m.addKrok(0, new KrokMerania(4, 8, 4, 7, 8, 9, 10));
-            m.addKrok(0, new KrokMerania(5, 9, 4, 7, 3, 2, 1));
-            foreach(KrokMerania k in m.getCyklus(0).getKroky())
-            {
-                Console.WriteLine(k.toString());
-            }
-            List<String> s=new List<string>();
-            //s[0]="dd";
+            Application.Run(new Form1());
+            //MeasurementParameters mp = new MeasurementParameters();
+            //mp.constant = 4;
+            //mp.endPoint = 5;
+            //mp.startPoint = 1;
+            //mp.name = "nazov";
+            //mp.pocetCyklov = 5;
+            //mp.stepTime = 10;
+            //mp.note = "poznamka ...";
+
+            //Meranie m = new Meranie(mp);
+            //m.addKrok(0, new KrokMerania(1, 2, 4, 7, 8, 9, 10));
+            //m.addKrok(0, new KrokMerania(2, 4, 4, 7, 8, 9, 10));
+            //m.addKrok(0, new KrokMerania(3, 5, 4, 7, 8, 9, 10));
+            //m.addKrok(0, new KrokMerania(4, 8, 4, 7, 8, 9, 10));
+            //m.addKrok(0, new KrokMerania(5, 9, 4, 7, 3, 2, 1));
+            //foreach (KrokMerania k in m.getCyklus(0).getKroky())
+            //{
+            //    Console.WriteLine(k.toString());
+            //}
+            //List<String> s = new List<string>();
+            //s[0] = "dd";
 
 
 
