5deb9afd01e62c85ec64293b5d5fc70c752cc634
drummer24 <martin.drumerpalka@gmail.com>
2016-01-01 10:19:07 +0100

    Update NoveMeranieWindow.cs

--- JDLMLab/JDLMLab/NoveMeranieWindow.cs
+++ JDLMLab/JDLMLab/NoveMeranieWindow.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,15 +20,18 @@ namespace JDLMLab
             {
             }
 
-            public ValidateParametersException(string message) : base(message)
+            public ValidateParametersException(string message)
+                : base(message)
             {
             }
 
-            public ValidateParametersException(string message, Exception innerException) : base(message, innerException)
+            public ValidateParametersException(string message, Exception innerException)
+                : base(message, innerException)
             {
             }
 
-            protected ValidateParametersException(SerializationInfo info, StreamingContext context) : base(info, context)
+            protected ValidateParametersException(SerializationInfo info, StreamingContext context)
+                : base(info, context)
             {
             }
         }
@@ -51,22 +54,23 @@ namespace JDLMLab
             stepTimeField2DMs.DisplayMember = "Key";
             stepTimeField2DMs.ValueMember = "Value";
 
-            
+
         }
         public MeasurementParameters parametreMerania;
 
-        
+
         private void runClick(object sender, EventArgs e)
         {
-            try {
+            try
+            {
                 nastavParametre();
                 DialogResult = DialogResult.OK; //okno sa zatvori a parameter parametreMerania bude pripraveny na meranie
             }
             catch (ValidateParametersException ex)
             {
-                MessageBox.Show(ex.Message.ToString(),"Chyba",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                MessageBox.Show(ex.Message.ToString(), "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            
+
 
         }
 
@@ -84,23 +88,23 @@ namespace JDLMLab
                     parametreMerania = new EnergyScanParameters(Convert.ToDouble(startPointFieldEs.Text), Convert.ToDouble(endPointFieldEs.Text), Convert.ToDouble(constantFieldEs.Text), Convert.ToDouble(stepTimeFieldEs.Text), Convert.ToInt32(pocetKrokovFieldEs.Text));
                     parametreMerania.setParameters(nameField
                         .Text, Convert.ToDouble(resolutionFieldEs.Text), Convert.ToInt32(pocetCyklovField.Value), noteField.Text);
-                    
+
                     ulozParametreEnergyScan();
                 }
                 catch (ValidateParametersException e)
                 {
                     throw e;
                 }
-                
-                
+
+
             }
             else if (typyMeraniaTaby.SelectedTab.Text.Equals("Mass Scan"))
             {
                 try
                 {
                     validateMassScanTab();
-                
-                
+
+
                     parametreMerania = new MassScanParameters(Convert.ToDouble(startPointFieldMs.Text), Convert.ToDouble(endPointFieldMs.Text), Convert.ToDouble(constantFieldMs.Text), (double)stepTimeFieldMs.SelectedValue);
                     parametreMerania.setParameters(nameField
                         .Text, Convert.ToDouble(resolutionFieldMs.Text), Convert.ToInt32(pocetCyklovField.Value), noteField.Text);
@@ -116,7 +120,7 @@ namespace JDLMLab
                 try
                 {
                     validateMass2DScanTab();
-                
+
                     EnergyScanParameters parametreMeraniaEnergy = new EnergyScanParameters(Convert.ToDouble(startPointField2DEs.Text), Convert.ToDouble(endPointField2DEs.Text), 0, (double)stepTimeFieldMs.SelectedValue, Convert.ToInt32(pocetKrokovField2DEs.Text));
 
 
@@ -153,28 +157,89 @@ namespace JDLMLab
 
         private void validateMass2DScanTab()
         {
-            //skontrolovat prislusne fieldy, pre kazdy jeho ocakavanu hodnotu.. ak nie su spravne vyhodit vynimku o chybe s konkretnou spravou, 
-            throw new ValidateParametersException("sprava o konretnej zlej hodnote");
+            Double startpoint;
+            Double endpoint;
+            Double resolution;
+            int step;
+            int krok;
+            if (namefield.Text.Length < 3)
+            {
+                ValidateParametersExceptio("Zadajte správny nazov merania");
+            }
+
+            else if (!Double.TryParse(startPointField2DMs.Text, out startpoint)) ValidateParametersException("Neplatná hodnota" + startPointField2DMs.Text);
+            else if (!Double.TryParse(endPointField2DMs.Text, out endpoint)) ValidateParametersException("Neplatná hodnota " + endPointField2DMs.Text);
+            else if (!Double.TryParse(resolutionField2DMs.Text, out resolution)) ValidateParametersException("Neplatná hodnota " + resolutionPointField2DMs.Text);
+            else if (!Double.TryParse(constantField2DMs.Text, out resolution)) ValidateParametersException("Neplatná hodnota " + constantPointField2DMs.Text);
+            else if (!Double.TryParse(stepTimefield2DMs, out step)) ValidateParametersException("Neplatná hodnota " + stepTimeFieldMs.Text);
+            else if (!Double.TryParse(pocetkrokovField2DMs.Text, out krok)) ValidateParametersException("Neplatná hodnota " + stepTimeFieldMs.Text);
+
+            else
+            {
+                return true;
+            }
+            return false;
         }
 
         private void validateMassScanTab()
         {
-            //...
-            throw new ValidateParametersException("sprava o konretnej zlej hodnote");
+            Double startpoint;
+            Double endpoint;
+            Double resolution;
+            int step;
+            int krok;
+            if (namefield.Text.Length < 3)
+            {
+                ValidateParametersExceptio("Zadajte správny nazov merania");
+            }
+
+            else if (!Double.TryParse(startPointFieldMs.Text, out startpoint)) ValidateParametersException("Neplatná hodnota" + startPointFieldMs.Text);
+            else if (!Double.TryParse(endPointFieldMs.Text, out endpoint)) ValidateParametersException("Neplatná hodnota " + endPointFieldMs.Text);
+            else if (!Double.TryParse(resolutionFieldMs.Text, out resolution)) ValidateParametersException("Neplatná hodnota " + resolutionPointFieldMs.Text);
+            else if (!Double.TryParse(constantFieldMs.Text, out resolution)) ValidateParametersException("Neplatná hodnota " + constantPointFieldMs.Text);
+            else if (!int.TryParse(stepTimeFieldMs.Text, out step)) ValidateParametersException("Neplatná hodnota " + stepTimeFieldMs.Text);
+            else if (!int.TryParse(pocetkrokovFieldMs.Text, out krok)) ValidateParametersException("Neplatná hodnota " + stepTimeFieldMs.Text);
+
+            else
+            {
+                return true;
+            }
+            return false;
+
         }
 
         private void validateEnergyScanTab()
         {
-            //...
-            throw new ValidateParametersException("sprava o konretnej zlej hodnote");
+            Double startpoint;
+            Double endpoint;
+            Double resolution;
+            int step;
+            int krok;
+            if (namefield.Text.Length < 3)
+            {
+                ValidateParametersExceptio("Zadajte správny nazov merania");
+            }
+
+            else if (!Double.TryParse(startPointFieldEs.Text, out startpoint)) ValidateParametersException("Neplatná hodnota" + startPointFieldEs.Text);
+            else if (!Double.TryParse(endPointFieldEs.Text, out endpoint)) ValidateParametersException("Neplatná hodnota " + endPointFieldEs.Text);
+            else if (!Double.TryParse(resolutionFieldEs.Text, out resolution)) ValidateParametersException("Neplatná hodnota " + resolutionPointFieldEs.Text);
+            else if (!Double.TryParse(constantFieldEs.Text, out resolution)) ValidateParametersException("Neplatná hodnota " + constantPointFieldEs.Text);
+            else if (!int.TryParse(stepTimeFieldEs.Text, out step)) ValidateParametersException("Neplatná hodnota " + stepTimeFieldEs.Text);
+            else if (!int.TryParse(pocetkrokovFieldEs.Text, out step)) ValidateParametersException("Neplatná hodnota " + stepTimeFieldEs.Text);
+
+            else
+            {
+                return true;
+            }
+            return false;
         }
 
-      
+
 
         private void NoveMeranieWindow_Load(object sender, EventArgs e)
         {
-            
-            
+
+
             //pre vsetky 3 taby, vyplnit hodnoty predvolene userom zo settings file
             //v subore maju prefix "e" pre energyscan, "m" pre massscan, 
             //"e2D" pre energy cast 2dscanu, a "m2D" pre mass scan cast 2dscanu
@@ -184,7 +249,7 @@ namespace JDLMLab
 
 
 
-            
+
 
         }
 
@@ -193,7 +258,9 @@ namespace JDLMLab
         {
             DialogResult = DialogResult.Cancel;
         }
+
+
     }
-    
-        
-    }
+
+
+}
